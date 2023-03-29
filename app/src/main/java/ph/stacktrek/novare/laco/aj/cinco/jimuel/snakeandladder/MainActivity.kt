30ae6cae6773e3dfdb18f40e1d560685ba92d747
Aj <arvi.laco@novare.com.hk>
2023-03-29 11:10:11 +0800

    added board fragment

--- app/src/main/java/ph/stacktrek/novare/laco/aj/cinco/jimuel/snakeandladder/MainActivity.kt
+++ app/src/main/java/ph/stacktrek/novare/laco/aj/cinco/jimuel/snakeandladder/MainActivity.kt
@@ -2,6 +2,8 @@ package ph.stacktrek.novare.laco.aj.cinco.jimuel.snakeandladder
 
 import android.os.Bundle
 import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageView
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val frameLayout = findViewById<FrameLayout>(R.id.home) // get a reference to the FrameLayout
+        val textView = TextView(this) // create a new TextView object
+        frameLayout.addView(textView) // add the TextView to the FrameLayout
 
         val startButton = findViewById<AppCompatButton>(R.id.start_button)
         val addButton = findViewById<AppCompatButton>(R.id.add_button)
@@ -29,6 +35,22 @@ class MainActivity : AppCompatActivity() {
 
             // Replace FragmentName with the name of your fragment class
             val fragment = leaderboard_fragment()
+            fragmentTransaction.replace(R.id.home, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        startButton.setOnClickListener {
+
+
+            startButton.visibility = View.GONE
+            addButton.visibility = View.GONE
+
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            // Replace FragmentName with the name of your fragment class
+            val fragment = BoardFragment()
             fragmentTransaction.replace(R.id.home, fragment)
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()

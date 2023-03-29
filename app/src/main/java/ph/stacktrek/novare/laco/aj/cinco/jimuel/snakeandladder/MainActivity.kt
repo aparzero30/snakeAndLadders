c3798618dbbaa392ec040c3d3eb6680ac6927a1d
aparzero30 <jimcin30@gmail.com>
2023-03-29 08:11:40 +0800

    Added fragment

--- app/src/main/java/ph/stacktrek/novare/laco/aj/cinco/jimuel/snakeandladder/MainActivity.kt
+++ app/src/main/java/ph/stacktrek/novare/laco/aj/cinco/jimuel/snakeandladder/MainActivity.kt
@@ -1,11 +1,26 @@
 package ph.stacktrek.novare.laco.aj.cinco.jimuel.snakeandladder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val trophyImageView = findViewById<AppCompatImageView>(R.id.trophy)
+
+        trophyImageView.setOnClickListener {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            // Replace FragmentName with the name of your fragment class
+            val fragment = leaderboard_fragment()
+            fragmentTransaction.replace(R.id.fragment_container_leaderboard, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
     }
 }

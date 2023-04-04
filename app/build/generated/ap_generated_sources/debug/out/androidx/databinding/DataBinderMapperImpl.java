b44229aa6398db27cc4e062c9b156db14f9d5091
Aj <arvi.laco@novare.com.hk>
2023-04-04 09:16:33 +0800

    fixed game icon

--- app/build/generated/ap_generated_sources/debug/out/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new ph.stacktrek.novare.laco.aj.cinco.jimuel.snakeandladder.DataBinderMapperImpl());
-  }
-}
--- app/build/generated/ap_generated_sources/debug/out/ph/stacktrek/novare/laco/aj/cinco/jimuel/snakeandladder/BR.java
+++
@@ -1,5 +0,0 @@
-package ph.stacktrek.novare.laco.aj.cinco.jimuel.snakeandladder;
-
-public class BR {
-  public static final int _all = 0;
-}
